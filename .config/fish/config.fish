97f162924b17815ee843ce9ca836d37207a0a114
Marcin Gazda <marcin.gazda@gmail.com>
2021-02-08 10:25:36 +0100

    Tuned fish colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,16 +3,23 @@ set -x TERM 'xterm-256color'
 
 fish_vi_key_bindings
 
-# Remove background from Windows folders
+### Autocomplete and highlight colors
+# set fish_color_normal brcyan
+set fish_color_autosuggestion '#928374'
+set fish_color_command '#076678'
+# set fish_color_error '#ff6c6b'
+set fish_color_param '#458588'
+
+### Remove background from Windows folders
 set -x LS_COLORS 'ow=34'
 
-# set PATH so it includes user's private bin if it exists
+### set PATH so it includes user's private bin if it exists
 if test -d $HOME/.local/bin
     set -x PATH "$HOME/.local/bin:$PATH"
 end
 
-# 'Bare git repo for dotfiles'
+### 'Bare git repo for dotfiles'
 alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-# Launch starship prompt
+### Launch starship prompt
 starship init fish | source
